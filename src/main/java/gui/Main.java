3d65893e914f495b032fec5b3bdb2995ff05be4d
Mieszko Maciura <DegradedColt73@outlook.com>
2021-03-28 00:23:44 +0100

    GuiManager class created and basic gui method added

---
+++ src/main/java/gui/Main.java
@@ -0,0 +1,22 @@
+package management;
+
+import gui.GuiManager;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        GuiManager guiManager = new GuiManager(stage);
+        guiManager.setAndShowGui();
+    }
+}
